238a41d26bdfa0bcd9a6c72350af4949b07d9dda
Sherry_Zhao <Sherry_Zhao@epam.com>
2020-10-30 17:14:48 +0800

    [3rd-course] Add unit tests for course list page

--- video-courses/src/app/containers/course-list/course-list-item/course-list-item.component.spec.ts
+++ video-courses/src/app/containers/course-list/course-list-item/course-list-item.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CourseListModule } from '../course-list.module';
+import { CourseListItem } from 'src/app/models';
+import { CourseListComponent } from '../course-list/course-list.component';
 
 import { CourseListItemComponent } from './course-list-item.component';
 
@@ -9,18 +10,27 @@ describe('CourseListItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CourseListItemComponent ]
+      declarations: [ CourseListItemComponent ],
+      providers: [ CourseListComponent ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+  it('should create', () => {
+    const courseItem: CourseListItem = {
+      id: 1,
+      title: 'Video Course 1. Name tag',
+      creationDate: '08/28/2020',
+      duration: 88,
+      description: "Learn about where you can find course descriptions.",
+      authors: ''
+    }
+
     fixture = TestBed.createComponent(CourseListItemComponent);
     component = fixture.componentInstance;
+    component.item = courseItem;
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component.item).toBe(courseItem);
   });
 });
