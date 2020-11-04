4512f1868f7002c8d487a632cb6eee152a119e72
Sherry_Zhao <Sherry_Zhao@epam.com>
2020-11-04 14:23:18 +0800

    fix(*): add log and set courses array property inside ngOnInit hook

--- video-courses/src/app/containers/course-list/course-list/course-list.component.ts
+++ video-courses/src/app/containers/course-list/course-list/course-list.component.ts
@@ -7,40 +7,39 @@ import { CourseListItem } from '../../../models';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-  public courseItems: CourseListItem[] = [
-    {
-      id: 1,
-      title: 'Video Course 1. Name tag',
-      creationDate: '08/28/2020',
-      duration: 88,
-      description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      authors: ''
-    },
-    {
-      id: 2,
-      title: 'Video Course 2. Name tag',
-      creationDate: '08/28/2020',
-      duration: 88,
-      description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      authors: ''
-    },
-    {
-      id: 3,
-      title: 'Video Course 3. Name tag',
-      creationDate: '08/28/2020',
-      duration: 88,
-      description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-      authors: ''
-    }
-  ];
-
+  courseItems: CourseListItem[];
   presentCourseItems: CourseListItem[];
 
-  constructor() {
-    this.presentCourseItems = this.courseItems
-  }
+  constructor() { }
 
   ngOnInit(): void {
+    this.courseItems = [
+      {
+        id: 1,
+        title: 'Video Course 1. Name tag',
+        creationDate: '08/28/2020',
+        duration: 88,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        authors: ''
+      },
+      {
+        id: 2,
+        title: 'Video Course 2. Name tag',
+        creationDate: '08/28/2020',
+        duration: 88,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        authors: ''
+      },
+      {
+        id: 3,
+        title: 'Video Course 3. Name tag',
+        creationDate: '08/28/2020',
+        duration: 88,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        authors: ''
+      }
+    ]
+    this.presentCourseItems = this.courseItems
   }
 
   removeItem(item) {
