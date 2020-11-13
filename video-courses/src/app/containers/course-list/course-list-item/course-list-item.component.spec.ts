deeeb09ae8d5f68e9f9583258266f1e64cb45b0e
Sherry_Zhao <Sherry_Zhao@epam.com>
2020-11-13 14:09:17 +0800

    fix: add unit tests for components

--- video-courses/src/app/containers/course-list/course-list-item/course-list-item.component.spec.ts
+++ video-courses/src/app/containers/course-list/course-list-item/course-list-item.component.spec.ts
@@ -7,6 +7,14 @@ import { CourseListItemComponent } from './course-list-item.component';
 describe('CourseListItemComponent', () => {
   let component: CourseListItemComponent;
   let fixture: ComponentFixture<CourseListItemComponent>;
+  const courseItem: CourseListItem = {
+    id: 1,
+    title: 'Video Course 1. Name tag',
+    creationDate: '08/28/2020',
+    duration: 88,
+    description: "Learn about where you can find course descriptions.",
+    authors: ''
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,23 +22,24 @@ describe('CourseListItemComponent', () => {
       providers: [ CourseListComponent ]
     })
     .compileComponents();
+    fixture = TestBed.createComponent(CourseListItemComponent);
+    component = fixture.componentInstance;
+    spyOn(console, "log");
   });
 
   it('should create', () => {
-    const courseItem: CourseListItem = {
-      id: 1,
-      title: 'Video Course 1. Name tag',
-      creationDate: '08/28/2020',
-      duration: 88,
-      description: "Learn about where you can find course descriptions.",
-      authors: ''
-    }
-
-    fixture = TestBed.createComponent(CourseListItemComponent);
-    component = fixture.componentInstance;
     component.item = courseItem;
     fixture.detectChanges();
 
     expect(component.item).toBe(courseItem);
+  });
+
+  it("should test deleteRequest function", () => {
+    component.item = courseItem;
+    let deleteButton = fixture.debugElement.nativeElement.querySelector("button.delete")
+    deleteButton.click();
+    fixture.detectChanges();
+
+    expect(console.log).toHaveBeenCalled();
   });
 });
