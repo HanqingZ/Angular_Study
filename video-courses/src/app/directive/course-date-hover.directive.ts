52a65868f233dc472b83351d67d132ab8cfc4ebe
Sherry_Zhao <Sherry_Zhao@epam.com>
2020-12-02 11:35:02 +0800

    Modify course date directive

--- video-courses/src/app/directive/course-date-hover.directive.ts
+++ video-courses/src/app/directive/course-date-hover.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 export class CourseDateHoverDirective {
   // @Input() createDate: Date;
   private TODAY: Date = new Date();
-  private FOURTEEN_DAYS_BEFORE: Date = new Date();
+  private FOURTEEN_DAYS_BEFORE: Date = new Date(this.TODAY.getFullYear(), this.TODAY.getMonth(), this.TODAY.getDate()-15);
 
   constructor(private eleRef: ElementRef, private renderer: Renderer2) { }
 
@@ -18,13 +18,25 @@ export class CourseDateHoverDirective {
 
     if(new Date(createDate) > this.TODAY) {
       console.log("Future");
-      // this.setBgColor("blue");
-      this.setBgColor("#c6def1");
+      this.setBgColor("blue");
+      this.renderer.setStyle(
+        this.eleRef.nativeElement,
+        'backgroundColor',
+        "#c6def1"
+      )
+      // this.eleRef.nativeElement.style.backgroundColor = "#c6def1";
+      // this.setBgColor("#c6def1");
     }
     if(tempCreateDate < this.TODAY && tempCreateDate >= this.FOURTEEN_DAYS_BEFORE){
       console.log("Recent");
-      // this.setBgColor("green");
-      this.setBgColor("#c9e4de");
+      this.setBgColor("green");
+      this.renderer.setStyle(
+        this.eleRef.nativeElement,
+        'backgroundColor',
+        "#c9e4de"
+      )
+      // this.eleRef.nativeElement.style.backgroundColor = "#c9e4de";
+      // this.setBgColor("#c9e4de");
     }
 
   }
@@ -37,6 +49,7 @@ export class CourseDateHoverDirective {
       'backgroundColor',
       color
     )
+    // this.eleRef.nativeElement.style.backgroundColor = color;
   }
 
 }
