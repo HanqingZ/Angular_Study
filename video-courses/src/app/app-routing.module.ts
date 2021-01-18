e31eba63f6c39f7ef5a216ee6568d83bad4f7f8d
Sherry_Zhao <Sherry_Zhao@epam.com>
2021-01-18 18:52:57 +0800

    Complete Debug page and modify router

--- video-courses/src/app/app-routing.module.ts
+++ video-courses/src/app/app-routing.module.ts
@@ -3,21 +3,33 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'courses',
     loadChildren: () => import('./containers/course-list/course-list.module').then(m => m.CourseListModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'courses',
     pathMatch: 'full'
   },
   {
-    path: 'addcourse',
+    path: 'courses/new',
+    loadChildren: () => import('./containers/add-course/add-course.module').then(m => m.AddCourseModule)
+  },
+  {
+    path: 'courses/:id',
     loadChildren: () => import('./containers/add-course/add-course.module').then(m => m.AddCourseModule)
   },
   {
     path: 'login',
     loadChildren: () => import('./containers/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'error',
+    loadChildren: () => import('./containers/debug/debug.module').then(m => m.DebugModule)
+  },
+  {
+    path: "**",
+    loadChildren: () => import('./containers/debug/debug.module').then(m => m.DebugModule)
   }
 ];
 
