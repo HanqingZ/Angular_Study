d7f0ea0d59b016724908255aa191ed81fd987e63
Sherry <sherry9277@gmail.com>
2021-01-18 10:21:33 +0800

    Modify login logic

--- video-courses/src/app/containers/login/login/login.component.ts
+++ video-courses/src/app/containers/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../../service/auth-service.service';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,25 @@ import { AuthServiceService } from '../../../service/auth-service.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email: String;
-  password: String;
-  loginFailed: Boolean = true;
+  email: string;
+  password: string;
+  loginFailed: Boolean = false;
 
-  constructor() { }
+  constructor(private authService: AuthServiceService) { }
 
   ngOnInit(): void {
 
   }
 
+  requestLogin () {
+    console.log(this.email, this.password);
+
+    Md5.hashStr(this.password)
+    const userInfo = {
+      email: this.email,
+      password: Md5.hashStr(this.password)
+    }
+    const result = this.authService.login(userInfo)
+    console.log(result.message);
+  }
 }
