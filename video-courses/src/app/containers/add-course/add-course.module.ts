7957e02dfb7f58f27954e16187f5d6806eb22632
Sherry_Zhao <Sherry_Zhao@epam.com>
2021-01-18 10:51:27 +0800

    Add-Course page

--- video-courses/src/app/containers/add-course/add-course.module.ts
+++ video-courses/src/app/containers/add-course/add-course.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< master
-<<<<<<< master
-=======
->>>>>>> Add routing for course list pages and complete relevant methods
-import { FormsModule } from '@angular/forms';
 import { NewCourseComponent } from './new-course/new-course.component';
-<<<<<<< master
-import { LayoutModule } from '../../layout/layout.module'
-<<<<<<< master
-=======
-import { NewCourseComponent } from './new-course/new-course.component';
->>>>>>> Add-Course page
-=======
->>>>>>> Modify breadcrumbs value
-=======
-import { LayoutModule } from '../../layout/layout.module';
-import { LoadingModule } from '../loading/loading.module'
->>>>>>> Add loading for all services all
 
 const routes: Routes = [
   {
@@ -33,28 +16,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-<<<<<<< master
-<<<<<<< master
-    RouterModule.forChild(routes),
-    FormsModule,
-    LayoutModule
-=======
     RouterModule.forChild(routes)
->>>>>>> Add-Course page
-=======
-    RouterModule.forChild(routes),
-<<<<<<< master
-    FormsModule
->>>>>>> Add routing for course list pages and complete relevant methods
-=======
-    FormsModule,
-<<<<<<< master
-    LayoutModule
->>>>>>> Modify breadcrumbs value
-=======
-    LayoutModule,
-    LoadingModule
->>>>>>> Add loading for all services all
   ],
   exports: [
     RouterModule,
--- video-courses/src/app/containers/add-course/new-course/new-course.component.ts
+++ video-courses/src/app/containers/add-course/new-course/new-course.component.ts
@@ -1,17 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< master
-<<<<<<< master
-import { Router, ActivatedRoute } from '@angular/router';
-import { CourseListItem } from 'src/app/models/course-list-item.model';
-import { CoursesService } from '../../../service/courses.service'
-=======
 import { CourseListItem } from 'src/app/models';
->>>>>>> Add-Course page
-=======
-import { Router, ActivatedRoute } from '@angular/router';
-import { CourseListItem } from 'src/app/models/course-list-item.model';
-import { CoursesService } from '../../../service/courses.service'
->>>>>>> Add routing for course list pages and complete relevant methods
 
 @Component({
   selector: 'app-new-course',
@@ -19,182 +7,16 @@ import { CoursesService } from '../../../service/courses.service'
   styleUrls: ['./new-course.component.scss']
 })
 export class NewCourseComponent implements OnInit {
-<<<<<<< master
-<<<<<<< master
-<<<<<<< master
-  id: number;
-  isAddMode: boolean;
-  pageTitle: string = "Courses";
-
-  courseSelected: CourseListItem = {
-    id: null,
-    name: '',
-    date: '',
-    length: null,
-    description: '',
-    authors: {
-      id: null,
-      name: null
-    },
-    isTopRated: false
-  };
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private coursesService: CoursesService
-  ) { }
-
-  ngOnInit(): void {
-    this.id = parseInt(this.route.snapshot.params['id']);
-    this.isAddMode = !this.id
-
-    if(!this.isAddMode) {
-      const courseEdit = this.coursesService.getItemById(this.id)[0]
-
-      if(!courseEdit) { this.router.navigate(['/debug']) }
-
-      this.courseSelected = courseEdit
-      const dates = courseEdit.date.split('/')
-      this.courseSelected.date = `${dates[2]}-${dates[0]}-${dates[1]}`;
-      this.pageTitle = `Courses / ${this.courseSelected.name}`
-    } else {
-      this.pageTitle = `Courses / New Course`
-    }
-  }
-
-  submitCourse() {
-    let result;
-    const dates = this.courseSelected.date.split('-')
-    this.courseSelected.date = `${dates[1]}/${dates[2]}/${dates[0]}`;
-
-    if(!this.courseSelected.name
-      || !this.courseSelected.description
-      || !this.courseSelected.length
-      || !this.courseSelected.date
-      || !this.courseSelected.authors
-    ) {
-      return console.log("Please Complete empty field")
-    }
-
-    if(!this.isAddMode){
-      result = this.coursesService.updateItem(this.courseSelected)
-      if(result.code === 0) {
-        console.log("success");
-        console.log(this.courseSelected);
-        this.router.navigate(['/courses'])
-      } else {
-        console.log("failed to modify this course");
-      }
-    } else {
-      this.courseSelected.id = this.coursesService.getCourseList.length
-      result = this.coursesService.createCourseItem(this.courseSelected)
-      if(result.code === 0) {
-        this.router.navigate(['/courses'])
-      } else {
-        console.log("failed to add this course");
-      }
-    }
-  }
-=======
   // public course: CourseListItem
   title: String;
   description: String;
   duration: Number;
   creationDate: Date;
   authors: String;
-=======
-  id: Number;
-=======
-  id: number;
->>>>>>> Implement load more function
-  isAddMode: boolean;
-<<<<<<< master
->>>>>>> Add routing for course list pages and complete relevant methods
-=======
-  pageTitle: string = "Courses";
-<<<<<<< master
->>>>>>> Modify breadcrumbs value
-=======
-  loading: boolean = true;
->>>>>>> Add loading for all services all
 
-  courseSelected: CourseListItem = {
-    id: null,
-    name: '',
-    date: '',
-    length: null,
-    description: '',
-    authors: {
-      id: null,
-      name: null
-    },
-    isTopRated: false
-  };
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private coursesService: CoursesService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.id = parseInt(this.route.snapshot.params['id']);
-    this.isAddMode = !this.id
-
-    if(!this.isAddMode) {
-      const courseEdit = this.coursesService.getItemById(this.id)[0]
-
-      if(!courseEdit) { this.router.navigate(['/debug']) }
-
-      this.courseSelected = courseEdit
-      const dates = courseEdit.date.split('/')
-      this.courseSelected.date = `${dates[2]}-${dates[0]}-${dates[1]}`;
-      this.pageTitle = `Courses / ${this.courseSelected.name}`
-    } else {
-      this.pageTitle = `Courses / New Course`
-    }
-
-    this.loading = false;
   }
 
-<<<<<<< master
->>>>>>> Add-Course page
-=======
-  submitCourse() {
-    let result;
-    const dates = this.courseSelected.date.split('-')
-    this.courseSelected.date = `${dates[1]}/${dates[2]}/${dates[0]}`;
-
-    if(!this.courseSelected.name
-      || !this.courseSelected.description
-      || !this.courseSelected.length
-      || !this.courseSelected.date
-      || !this.courseSelected.authors
-    ) {
-      return console.log("Please Complete empty field")
-    }
-
-    this.loading = true;
-    if(!this.isAddMode){
-      result = this.coursesService.updateItem(this.courseSelected)
-      if(result.code === 0) {
-        console.log("success");
-        console.log(this.courseSelected);
-        this.router.navigate(['/courses'])
-      } else {
-        console.log("failed to modify this course");
-      }
-    } else {
-      this.courseSelected.id = this.coursesService.getCourseList.length
-      result = this.coursesService.createCourseItem(this.courseSelected)
-      if(result.code === 0) {
-        this.router.navigate(['/courses'])
-      } else {
-        console.log("failed to add this course");
-      }
-    }
-    this.loading = false;
-  }
->>>>>>> Add routing for course list pages and complete relevant methods
 }
