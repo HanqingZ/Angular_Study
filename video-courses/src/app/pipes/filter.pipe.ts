8309a47fd6f0290829bf3c8d40b8ebe4d856bef3
Sherry_Zhao <Sherry_Zhao@epam.com>
2021-01-18 13:34:58 +0800

    Modify with onPush

--- video-courses/src/app/pipes/filter.pipe.ts
+++ video-courses/src/app/pipes/filter.pipe.ts
@@ -7,21 +7,15 @@ import { CourseListItem } from '../models';
 export class FilterPipe implements PipeTransform {
 
   transform(courseList: Array<CourseListItem>, courseName: string): Array<CourseListItem> {
-    console.log("filter pipe", courseName);
     let tmpCourseList: Array<CourseListItem>;
-
     if(courseName && courseList.length !== 0) {
-      console.log("in filter");
 
       tmpCourseList = courseList.filter(ele =>
         ele.title.toUpperCase().includes(courseName.toUpperCase().trim())
       )
-      console.log(tmpCourseList);
-
     } else {
       tmpCourseList = courseList;
     }
-    console.log(tmpCourseList);
 
     return tmpCourseList;
   }
