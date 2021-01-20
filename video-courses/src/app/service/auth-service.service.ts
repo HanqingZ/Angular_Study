26fae7bbe835dc78e0388bc14aa7790436e623d3
Sherry_Zhao <Sherry_Zhao@epam.com>
2021-01-20 20:01:00 +0800

    Add login method

--- video-courses/src/app/service/auth-service.service.ts
+++ video-courses/src/app/service/auth-service.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { UserModule } from '../models/user.model';
+import { TokenModel } from '../models/token.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,29 +12,33 @@ export class AuthService {
   existUser: UserModule[];
   currentUser: UserModule = null;
 
-  constructor() {
-    this.existUser = [
-      {
-        id: 1,
-        firstName: "David",
-        lastName: "Minsk",
-        email: "example@gmail.com",
-        password: "5f4dcc3b5aa765d61d8327deb882cf99"
-      }
-    ]
+  constructor(private http: HttpClient) {
+    // this.existUser = [
+    //   {
+    //     id: 1,
+    //     name: {
+    //       firstName: "David",
+    //       lastName: "Minsk"
+    //     },
+    //     token: "jfkdlangvfaklvfdna",
+    //     login: "example@gmail.com",
+    //     password: "5f4dcc3b5aa765d61d8327deb882cf99"
+    //   }
+    // ]
   }
 
-  login(userInfo) {
+  login(userInfo): Observable<Object> {
     if (userInfo?.email && userInfo?.password) {
-      for(let i = 0; i < this.existUser.length; i++) {
-        const ele = this.existUser[i]
-        if(ele.email == userInfo.email && userInfo.password == ele.password) {
-          this.currentUser = ele;
-          return { code: 0, message:`logged in successfully` }
-        }
+      const result = this.http.post(
+        `${environment.apiBaseUrl}auth/login`,
+        {login: userInfo.email, password: userInfo.password}
+      )
+      if(result) {
+        return result
       }
+      // }
     }
-    return { code: 400, message: "error" }
+    return null;
   }
 
   logout() {
@@ -44,7 +51,10 @@ export class AuthService {
     return true;
   }
 
-  getUserInfo(): UserModule {
-    return this.currentUser;
+  getUserInfo(token): void {
+    const result = this.http.post(
+      `${environment.apiBaseUrl}auth/userinfo`,
+      { token }
+    )
   }
 }
