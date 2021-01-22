1b069b7d431ac3a0e0bf03f3a3d4d375cc4bfeea
Sherry_Zhao <Sherry_Zhao@epam.com>
2021-01-22 17:39:38 +0800

    Add debounce

--- video-courses/src/app/containers/course-list/search-control/search-control.component.ts
+++ video-courses/src/app/containers/course-list/search-control/search-control.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-control',
@@ -12,7 +13,7 @@ export class SearchControlComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.searchValue$.pipe().subscribe((event)=>{
+    this.searchValue$.pipe(debounce(() => timer(1000))).subscribe((event)=>{
       let searchValue = (<HTMLInputElement>event.target).value;
       if(searchValue.length >= 3){
         this.searchItem.emit(searchValue);
